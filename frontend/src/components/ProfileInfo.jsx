8cea0fa58ae7d59a23b6bf9c993cf3bb1b8d800b
rishantkohli2033 <rishantkohli2033@gmail.com>
2024-03-03 03:12:28 +0530

    completed GET user info

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -3,23 +3,11 @@ import { RiGitRepositoryFill, RiUserFollowFill, RiUserFollowLine } from "react-i
 import { FaXTwitter } from "react-icons/fa6";
 import { TfiThought } from "react-icons/tfi";
 import { FaEye } from "react-icons/fa";
+import {formatMemberSince} from "../utils/functions";
 
-const ProfileInfo = () => {
-	const userProfile = {
-		avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
-		bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
-		email: "johndoe@gmail.com",
-		followers: 100,
-		following: 200,
-		html_url: "https://github.com/rishantkohli2033",
-		location: "Somewhere, Earth",
-		name: "John Doe",
-		public_gists: 100,
-		public_repos: 100,
-		twitter_username: "johndoe",
-		login: "johndoe",
-	};
-
+const ProfileInfo = ({userProfile}) => {
+	
+	const memberSince = formatMemberSince(userProfile?.created_at);
 	return (
 		<div className='lg:w-1/3 w-full flex flex-col gap-2 md:sticky md:top-10'>
 			<div className='bg-glass rounded-lg p-4'>
@@ -31,7 +19,7 @@ const ProfileInfo = () => {
 					{/* View on Github */}
 					<div className='flex gap-2 items-center flex-col'>
 						<a
-							href={userProfile.html_url}
+							href={userProfile?.html_url}
 							target='_blank'
 							rel='noreferrer'
 							className='bg-glass font-medium w-full text-xs p-2 rounded-md cursor-pointer border border-blue-400 flex items-center gap-2'
@@ -46,7 +34,7 @@ const ProfileInfo = () => {
 				{userProfile?.bio ? (
 					<div className='flex items-center gap-2'>
 						<TfiThought />
-						<p className='text-sm'>{userProfile?.bio.substring(0, userProfile.bio.length)}...</p>
+						<p className='text-sm'>{userProfile?.bio.substring(0, 60)}...</p>
 					</div>
 				) : null}
 
@@ -61,7 +49,7 @@ const ProfileInfo = () => {
 				{/* Twitter Username */}
 				{userProfile?.twitter_username ? (
 					<a
-						href={`https://twitter.com/${userProfile.twitter_username}`}
+						href={`https://twitter.com/${userProfile?.twitter_username}`}
 						target='_blank'
 						rel='noreferrer'
 						className='flex items-center gap-2 hover:text-sky-500'
@@ -74,14 +62,14 @@ const ProfileInfo = () => {
 				{/* Member Since Date */}
 				<div className='my-2'>
 					<p className='text-gray-600 font-bold text-sm'>Member since</p>
-					<p className=''>21 Sep, 2023</p>
+					<p className=''>{memberSince}</p>
 				</div>
 
 				{/* Email Address */}
 				{userProfile?.email && (
 					<div className='my-2'>
 						<p className='text-gray-600 font-bold text-sm'>Email address</p>
-						<p className=''>{userProfile.email}</p>
+						<p className=''>{userProfile?.email}</p>
 					</div>
 				)}
 
